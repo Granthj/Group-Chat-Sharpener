e92b40093a64e9d516f62e26c161ca4710d713b8
Granthj <dasgranth24@gmail.com>
2026-05-12 20:07:13 +0530

    added ws or sockjs

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const path = require('path');
+const http = require('http');
+const wss = require('sockjs');
 const db = require('./Utils/db');
 const ConversationParticipants = require('./Model/conversationParticipantsSchema');
 const Conversation = require('./Model/conversationSchema');
@@ -14,36 +16,98 @@ const apiRoutes = require('./Routes/apiRoutes');
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname,"../Frontend")));
+app.use(express.static(path.join(__dirname, "../Frontend")));
 
 app.use('/api', apiRoutes);
 
-app.use((req,res)=>{
-    res.sendFile(path.join(__dirname,'../Frontend/index.html'));
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname, '../Frontend/index.html'));
     // res.sendFile(path.join(__dirname,'Frontend','index.html'));
 });
 
 // associations
 
-Conversation.hasMany(Message,{foreignKey:'conversationId'});
-Message.belongsTo(Conversation,{foreignKey:'conversationId'});
+Conversation.hasMany(Message, { foreignKey: 'conversationId' });
+Message.belongsTo(Conversation, { foreignKey: 'conversationId' });
 
-User.hasMany(Message,{foreignKey:'senderId'});
-Message.belongsTo(User,{foreignKey:'senderId'});
+User.hasMany(Message, { foreignKey: 'senderId' });
+Message.belongsTo(User, { foreignKey: 'senderId' });
 
 Conversation.belongsToMany(User, {
-    through:ConversationParticipants,
-    foreignKey:'conversationId'
+    through: ConversationParticipants,
+    foreignKey: 'conversationId'
 });
-User.belongsToMany(Conversation,{
-    through:ConversationParticipants,
-    foreignKey:'userId'
+User.belongsToMany(Conversation, {
+    through: ConversationParticipants,
+    foreignKey: 'userId'
 });
 
+const server = http.createServer(app);
+const socketServer = wss.createServer();
+const client = {};
 
+socketServer.on('connection', (conn) => {
+    console.log('Websocket connected');
+
+    const connectionId = conn.id;
+
+    clients[connectionId] = conn;
+
+
+    conn.on('data', async (message) => {
+
+        /*
+        Example incoming message:
+    
+        {
+          senderId:1,
+          receiverId:2,
+          text:"hello"
+        }
+        */
+
+        const parsedMessage = JSON.parse(message);
+
+        console.log(parsedMessage);
+
+        const savedMessage =
+            await Message.create({
+    
+                conversationId:
+                    parsedMessage.conversationId,
+    
+                senderId:
+                    parsedMessage.senderId,
+    
+                text:
+                    parsedMessage.text
+    
+            });
+
+    Object.values(clients).forEach(client => {
+
+        client.write(
+          JSON.stringify(savedMessage)
+        );
+
+      });
+
+  });
+    conn.on('close', () => {
+
+    console.log('WebSocket Disconnected');
+
+    delete clients[connectionId];
+
+  });
+
+});
+sockServer.installHandlers(server, {
+  prefix: '/chat'
+});
 const PORT = process.env.PORT || 5000;
 
-db.sync().then(()=>{
+db.sync().then(() => {
     app.listen(5000, () => {
         console.log(`Server is running on port ${PORT}`);
     });
